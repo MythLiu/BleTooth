371b8b88935ecae5ffb5a7b6a8419909570bbd28
LABELNET <1406046087@qq.com>
2017-02-05 15:51:58 +0800

    初始化项目

---
+++ bletooth/src/main/java/cn/labelnet/bletooth/BleTooth.java
@@ -0,0 +1,17 @@
+package cn.labelnet.bletooth;
+
+/**
+ * @Package cn.labelnet.bletooth
+ * <p>
+ * @Author yuan
+ * @Blog http://blog.csdn.net/lablenet
+ * <p>
+ * @Date Created in 3:27 PM 2/5/2017
+ * @Desc Desc
+ */
+
+public class BleTooth {
+
+
+
+}

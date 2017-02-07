60e2e1d2e40d7ccd2ad7def3a386e57dfc3cd2eb
LABELNET <1406046087@qq.com>
2017-02-07 15:35:24 +0800

    【EDIT】Ble Perpheral 判断实现

--- bletooth/src/main/java/cn/labelnet/bletooth/le/LeBlueTooth.java
+++ bletooth/src/main/java/cn/labelnet/bletooth/le/LeBlueTooth.java
@@ -1,5 +1,14 @@
 package cn.labelnet.bletooth.le;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.content.Context;
+import android.os.Build;
+
+import cn.labelnet.bletooth.ble.BleBlueTooth;
+
 /**
  * @Package cn.labelnet.bletooth.le
  * <p>
@@ -11,4 +20,74 @@ package cn.labelnet.bletooth.le;
  */
 
 public class LeBlueTooth {
+
+    private Context mContext;
+
+    //bluetooth
+    private BluetoothAdapter mBluetoothAdapter;
+
+    //scan
+    private BluetoothLeScanner mBluetoothLeScaner;
+    private BluetoothLeAdvertiser mBluetoothLeAdvertiser;
+
+
+    private static LeBlueTooth mInstance;
+
+    /**
+     * init
+     *
+     * @param context applicationContext
+     * @return instance
+     */
+    public static LeBlueTooth getInstance(Context context) {
+        if (mInstance == null) {
+            synchronized (BleBlueTooth.class) {
+                if (mInstance == null) {
+                    mInstance = new LeBlueTooth(context);
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    public LeBlueTooth(Context context) {
+        this.mContext = context.getApplicationContext();
+        initBlueTooth();
+    }
+
+    /**
+     * System VERSION
+     *
+     * @return API is >=21
+     */
+    private boolean isBuildLOLLIPOP() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    private void initBlueTooth() {
+        final BluetoothManager mBluetoothManager = (BluetoothManager) mContext.getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
+        if (isBuildLOLLIPOP()) {
+            mBluetoothLeScaner = mBluetoothAdapter.getBluetoothLeScanner();
+            mBluetoothLeAdvertiser = mBluetoothAdapter.getBluetoothLeAdvertiser();
+        }
+    }
+
+    /**
+     * Support BLE Perpheral
+     *
+     * @return is support
+     */
+    public boolean isCheckSupportPerpheral() {
+        if (isBuildLOLLIPOP()) {
+            return mBluetoothAdapter.isMultipleAdvertisementSupported();
+        }
+        return false;
+    }
+
+    //========================================== SCAN ==============================================
+
+
+
+
 }

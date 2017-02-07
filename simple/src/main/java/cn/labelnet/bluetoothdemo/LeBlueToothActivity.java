406e60870dc8e7c9de8a03e03df855d0a3bc82e7
LABELNET <1406046087@qq.com>
2017-02-07 19:03:48 +0800

    【EDIT】测试5.0蓝牙扫描

--- simple/src/main/java/cn/labelnet/bluetoothdemo/LeBlueToothActivity.java
+++ simple/src/main/java/cn/labelnet/bluetoothdemo/LeBlueToothActivity.java
@@ -1,13 +1,145 @@
 package cn.labelnet.bluetoothdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.labelnet.bletooth.ble.bean.BleDevice;
+import cn.labelnet.bletooth.le.LeBlueTooth;
+import cn.labelnet.bletooth.le.scan.LeScanResultCallBack;
+import cn.labelnet.bletooth.util.LogUtil;
 
 public class LeBlueToothActivity extends AppCompatActivity {
+
+
+    private static TextView tv;
+    private LeBlueTooth leBlueTooth;
+    private LeScanSimpleCallBack leScanSimpleCallBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_le_blue_tooth);
+
+        leBlueTooth = new LeBlueTooth(getApplicationContext());
+        leScanSimpleCallBack = new LeScanSimpleCallBack(5000);
+        tv = (TextView) findViewById(R.id.tv_data);
+
+        findViewById(R.id.btn_scan_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leBlueTooth.startScan(leScanSimpleCallBack);
+            }
+        });
+
+        findViewById(R.id.btn_scan_stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leBlueTooth.stopScan(leScanSimpleCallBack);
+            }
+        });
+
     }
+
+
+    private static class LeScanSimpleCallBack extends LeScanResultCallBack {
+
+
+        private List<BleDevice> bleDevices;
+
+        public LeScanSimpleCallBack(long timeOutMill) {
+            super(timeOutMill);
+        }
+
+        @Override
+        protected void onScanDevicesData(List<BleDevice> bleDevices) {
+            this.bleDevices = bleDevices;
+            updateInfo();
+        }
+
+        @Override
+        protected void onNotifyBleToothDeviceRssi(int position, int rssi) {
+            LogUtil.v("----------------------------------------------------Position : " + position + " Rssi : " + rssi);
+            if (position > -1) {
+                BleDevice bleDevice = bleDevices.get(position);
+                bleDevice.setRssi(rssi);
+                bleDevices.set(position, bleDevice);
+                updateInfo();
+            }
+        }
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            LogUtil.v("DeviceName : " + result.getDevice().getName() + " DeviceMac: " + result.getDevice().getAddress());
+        }
+
+        @Override
+        protected void bleToothScanProcess(float process) {
+            //进度
+            LogUtil.v("Scan Process : " + process);
+        }
+
+        @Override
+        protected void onScanStatus(int errorCode) {
+            //状态
+            printStatus(errorCode);
+        }
+
+        @Override
+        public List<ScanFilter> getScanFilters() {
+            //过滤
+            //CC2650 SensorTag
+            //SimpleBLEPeripheral
+            List<ScanFilter> scanFilters = new ArrayList<>();
+            ScanFilter filter1 = new ScanFilter
+                    .Builder()
+                    .setDeviceName("SimpleBLEPeripheral")
+                    .build();
+            scanFilters.add(filter1);
+            return scanFilters;
+        }
+
+        @Override
+        public ScanSettings getScanSettings(ScanSettings.Builder builder) {
+            //配置
+            return super.getScanSettings(builder);
+        }
+
+        private void updateInfo() {
+            if (bleDevices.size() > 0) {
+                tv.setText(bleDevices.toString());
+            } else {
+                tv.setText("没有设备");
+            }
+        }
+
+        private void printStatus(int code) {
+            String msg = "来自ScanCallBack";
+            switch (code) {
+                case 100:
+                    msg = "scan start";
+                    break;
+                case 101:
+                    msg = "scan end";
+                    break;
+                case 102:
+                    msg = "scan stop";
+                    break;
+                case 103:
+                    msg = "scan timeout";
+                    break;
+            }
+            LogUtil.v("Scan Status ： " + msg);
+        }
+    }
+
+
 }
